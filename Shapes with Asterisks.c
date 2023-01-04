5c26cba023e13ec07c118c4032d4107b5b54c2fa
jacobmartin26 <121983988+jacobmartin26@users.noreply.github.com>
2023-01-04 17:48:38 -0500

    Add files via upload

---
+++ Shapes with Asterisks.c
@@ -0,0 +1,30 @@
+// Programmer:		Jacob Martin
+// Date:			2/1/21
+// Program Name:	Shapes with Asterisks
+// Chapter:			Chapter # - Chapter name
+// Description:		This program makes use of printf statements in order to display four different shapes. Since printf's display exactly what is in quotation marks,
+//					the programmer has to manually enter every space, *, and | in order to display the desired shape(s).
+
+#define _CRT_SECURE_NO_WARNINGS // Disable warnings (and errors) when using non-secure versions of printf, scanf, strcpy, etc.
+#include <stdio.h> // Needed for working with printf and scanf
+
+int main(void)
+{
+	// Constant and Variable Declarations
+
+
+
+	// *** Your program goes here ***
+	printf("  *********  |     ***     |      *      |      *      \n");
+	printf("  *       *  |   *     *   |     ***     |     * *     \n");
+	printf("  *       *  |  *       *  |    *****    |    *   *    \n");
+	printf("  *       *  |  *       *  |      *      |   *     *   \n");
+	printf("  *       *  |  *       *  |      *      |  *       *  \n");
+	printf("  *       *  |  *       *  |      *      |   *     *   \n");
+	printf("  *       *  |  *       *  |      *      |    *   *    \n");
+	printf("  *       *  |   *     *   |      *      |     * *     \n");
+	printf("  *********  |     ***     |      *      |      *      \n");
+	printf("\n");
+
+	return 0;
+} // end main()
